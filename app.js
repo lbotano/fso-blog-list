32c0fde2434d63ed6912aaae8c914d5ce6fbc238
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-04-14 17:49:51 -0300

    Completed exercises 4.1.-4.2.

---
+++ app.js
@@ -0,0 +1,20 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const app = express()
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
